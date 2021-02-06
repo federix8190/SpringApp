f8302032c0a0a6fe5b03e4226aaa59612de86539
Federico Torres <federix.8190@gmail>
2021-02-06 11:10:37 -0300

    Se agrega modelo de prueba

--- src/main/java/com/core/federix/controllers/TestController.java
+++ src/main/java/com/core/federix/controllers/TestController.java
@@ -1,5 +1,6 @@
 package com.core.federix.controllers;
 
+import com.core.federix.models.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/test")
@@ -24,9 +28,23 @@ public class TestController {
 		return "Holaaa";
 	}
 
-	/*@PostMapping
-	public Answer createAnswer(@RequestHeader("Authorization") String authorization, @RequestBody Answer answer) {
-		return answerService.addAnswersToPerson(jwtUtil.getEmailFromJwtToken(authorization), answer);
-	}*/
+	@GetMapping(value="/persons")
+	public List<Persona> persons() {
+		List<Persona> res = new ArrayList<>();
+		res.add(new Persona(1, "Juan", "Perez", 18));
+		res.add(new Persona(2, "Fede", "Torres", 31));
+		res.add(new Persona(3, "Carlos", "Gonzalez", 46));
+		res.add(new Persona(1, "Jazmin", "Cubilla", 25));
+		return res;
+	}
+
+	/**
+	 * Para probar --> {"id":1,"edad":20,"nombre":"luis","apellido":""}
+	 * @param p
+	 */
+	@PostMapping(value="/persons")
+	public void save(@RequestBody Persona p) {//@RequestHeader("Authorization") String authorization) {
+		System.err.println("Persona : " + p);
+	}
 
 }
